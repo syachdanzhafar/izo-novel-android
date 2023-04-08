b18880dff270b32c22d8572a17f4b41220cb7063
sata lesmana <lesmanasata@gmail.com>
2023-04-08 15:30:20 +0700

    form input activity

---
+++ app/src/main/java/com/sata/izonovel/FormInptActivity.java
@@ -0,0 +1,14 @@
+package com.sata.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormInptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_inpt);
+    }
+}
--- app/src/main/java/com/sata/izonovel/MainActivity.java
+++ app/src/main/java/com/sata/izonovel/MainActivity.java
@@ -2,6 +2,7 @@ package com.sata.izonovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,17 +10,18 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout menuOne;
+    LinearLayout menuInfoPengguna;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menuOne = findViewById(R.id.menu_one);
-        menuOne.setOnTouchListener(new View.OnTouchListener() {
+        menuInfoPengguna = findViewById(R.id.informasi_pengguna);
+        menuInfoPengguna.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Toast.makeText(MainActivity.this, "Meu satu",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, BiodataActivity.class);
+                startActivity(intent);
                 return false;
             }
         });
