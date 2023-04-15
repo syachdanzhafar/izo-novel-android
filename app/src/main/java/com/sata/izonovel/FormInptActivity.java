3f69ba5b45b3ef6bca39c42ba380d967f47c96b9
sata lesmana <lesmanasata@gmail.com>
2023-04-15 13:13:08 +0700

    form input activity

--- app/src/main/java/com/sata/izonovel/FormInptActivity.java
+++ app/src/main/java/com/sata/izonovel/FormInptActivity.java
@@ -2,13 +2,24 @@ package com.sata.izonovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class FormInptActivity extends AppCompatActivity {
+    String[] genre = {"Action", "Romance", "Fantasi", "Horor", "Comedy", "Sci-fi"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_inpt);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.select_dialog_item, genre);
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.filled_exposed_dropdown);
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+
     }
 }
