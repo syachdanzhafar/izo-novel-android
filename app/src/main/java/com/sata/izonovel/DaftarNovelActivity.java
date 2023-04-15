d0e202bc3aec87ce15f68431db4e3e8e78fb1091
SyachdanZhafar <73096174+syachdanzhafar@users.noreply.github.com>
2023-04-15 16:49:20 +0700

    Tambah activity_daftar_novel.xml

---
+++ app/src/main/java/com/sata/izonovel/DaftarNovelActivity.java
@@ -0,0 +1,14 @@
+package com.sata.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarNovelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_novel);
+    }
+}
